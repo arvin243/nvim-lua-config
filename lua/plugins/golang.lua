2e4d7a9e9823065421e8df94d24727a1ca178757
qi.guo <qi.guo@shopee.com>
2023-07-21 16:06:38 +0800

    mod use ray-x/go & gofumpt to format

--- lua/plugins/golang.lua
+++ lua/plugins/golang.lua
@@ -1,6 +1,6 @@
 return {
   "ray-x/go.nvim",
-  ft = { "go", 'gomod' },
+  ft = { "go", "gomod", "gowork", "gotmpl" },
   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   dependencies = {                                        -- optional packages
     "ray-x/guihua.lua",
@@ -9,13 +9,20 @@ return {
   },
   config = function()
     require("go").setup({
+      gofmt = "gofumpt",
+      fillstruct = "gopls",
+      lsp_gofumpt = true,
+      lsp_codelens = true,
+      lsp_inlay_hints = {
+        enable = false,
+      }
     })
 
-    local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+    local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
     vim.api.nvim_create_autocmd("BufWritePre", {
       pattern = "*.go",
       callback = function()
-        require('go.format').goimport()
+        require('go.format').gofmt()
       end,
       group = format_sync_grp,
     })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,7 +2,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local util = require "lspconfig/util"
 
       -- refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
@@ -13,8 +13,8 @@ return {
         end
       })
 
-      require('lspconfig').gopls.setup({
-        cmd = { 'gopls', 'serve' },
+      require("lspconfig").gopls.setup({
+        cmd = { "gopls", "serve" },
         fieltypes = { "go", "gomod", "gowork", "gotmpl" },
         root_dir = util.root_pattern("go.work", "go.mod", ".git"),
         settings = {
@@ -39,23 +39,24 @@ return {
           },
         },
       })
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        pattern = '*.go',
-        callback = function()
-          vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
-        end
-      })
+      -- -- gofumpt on save
+      -- vim.api.nvim_create_autocmd('BufWritePre', {
+      --   pattern = '*.go',
+      --   callback = function()
+      --     vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+      --   end
+      -- })
 
       vim.keymap.set("n", "<leader>do", "<cmd>lua vim.diagnostic.open_float()<CR>")
       vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
       vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
 
-      require 'lspconfig'.lua_ls.setup {
+      require("lspconfig").lua_ls.setup {
         capabilities = capabilities,
       }
-      require 'lspconfig'.marksman.setup {}
-      require 'lspconfig'.pyright.setup {}
-      require 'lspconfig'.rust_analyzer.setup {}
+      require("lspconfig").marksman.setup {}
+      require("lspconfig").pyright.setup {}
+      require("lspconfig").rust_analyzer.setup {}
     end
   },
   {
