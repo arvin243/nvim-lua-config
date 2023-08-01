214e3b7b43ccda45cb604226e5dca226a7f032a4
qi.guo <qi.guo@shopee.com>
2023-08-01 15:57:08 +0800

    fix telescope local var name

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -13,17 +13,17 @@ return {
       },
     },
     config = function()
-      local telescope = require('telescope.builtin')
-      vim.keymap.set("n", "<leader>fg", telescope.live_grep, {})
-      vim.keymap.set("n", "<leader>sf", telescope.live_grep, {})
-      vim.keymap.set("n", "<leader>bb", telescope.buffers, {})
-      vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
-      vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
-      vim.keymap.set('n', '<leader>fh', telescope.help_tags, {})
-      vim.keymap.set('n', '<leader>dd', telescope.diagnostics, {})
-      vim.keymap.set('n', '<leader>ts', telescope.treesitter, {})
-      vim.keymap.set("n", "gr", telescope.lsp_references, {})
-      vim.keymap.set("n", "gb", telescope.git_branches, {})
+      local builtin = require('telescope.builtin')
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>sf", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>bb", builtin.buffers, {})
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+      vim.keymap.set('n', '<leader>dd', builtin.diagnostics, {})
+      vim.keymap.set('n', '<leader>ts', builtin.treesitter, {})
+      vim.keymap.set("n", "gr", builtin.lsp_references, {})
+      vim.keymap.set("n", "gb", builtin.git_branches, {})
 
       require('telescope').setup {
         defaults = {
