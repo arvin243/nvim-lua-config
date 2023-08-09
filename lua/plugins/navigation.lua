de9f41721fd824857ef93017174cf0986dad16ce
qi.guo <qi.guo@shopee.com>
2023-08-09 12:10:37 +0800

    mod telescope config

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -9,30 +9,59 @@ return {
       {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = "make",
-        config = function() require("telescope").load_extension("fzf") end,
+        config = function()
+          require("telescope").load_extension("fzf")
+        end
       },
     },
     config = function()
+      local keymap = vim.keymap
       local builtin = require('telescope.builtin')
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>sf", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>bb", builtin.buffers, {})
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-      vim.keymap.set('n', '<leader>dd', builtin.diagnostics, {})
-      vim.keymap.set('n', '<leader>ts', builtin.treesitter, {})
-      vim.keymap.set("n", "gr", builtin.lsp_references, {})
-      vim.keymap.set("n", "gb", builtin.git_branches, {})
+      keymap.set('n', '<leader>ff', builtin.find_files, {})
+      keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      keymap.set('n', '<leader>fb', builtin.buffers, {})
+      keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-      require('telescope').setup {
+      keymap.set("n", "<leader>sf", builtin.live_grep, {})
+      keymap.set("n", "<leader>bb", builtin.buffers, {})
+      keymap.set('n', '<leader>dd', builtin.diagnostics, {})
+      keymap.set('n', '<leader>ts', builtin.treesitter, {})
+      keymap.set("n", "gr", builtin.lsp_references, {})
+      keymap.set("n", "gb", builtin.git_branches, {})
+
+      local actions = require('telescope.actions')
+      require('telescope').setup({
         defaults = {
-          mappings = {
-            n = { ['<c-d>'] = require('telescope.actions').delete_buffer },
-            i = { ['<c-d>'] = require('telescope.actions').delete_buffer },
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--fixed-strings",
+            "--smart-case",
+            "--trim",
+          },
+          layout_config = {
+            width = 0.8,
+            height = 0.8,
+          },
+          file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+          grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+        },
+        pickers = {
+          buffers = {
+            show_all_buffers = true,
+            sort_lastused = true,
+            mappings = {
+              i = {
+                ["<c-d>"] = actions.delete_buffer,
+              },
+            }
           },
         },
-      }
+      })
     end
-  },
+  }
 }
